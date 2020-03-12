b56c90d70ef29c8747b2753cb8c29ffd7b1fc4d0
Mikhail Vyshinskiy <mg.vyshinskiy@gmail.com>
2020-03-13 00:32:21 +0300

    Spring Boot initialization

---
+++ src/main/java/casa/squawk7777/jquiz/JquizApplication.java
@@ -0,0 +1,13 @@
+package casa.squawk7777.jquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JquizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/casa/squawk7777/jquiz/JquizApplicationTests.java
@@ -0,0 +1,13 @@
+package casa.squawk7777.jquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
